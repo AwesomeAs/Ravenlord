3a41520ac92a1fde920ec5c0f97646b9ec009b49
AwesomeAs <bloxxer9001@gmail.com>
2016-02-12 11:01:14 +0100

    Added new drawables Grass and CobbleWall

---
+++ rpg/graphic/map/CobbleWall.java
@@ -0,0 +1,24 @@
+package graphic.map;
+
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
+import feature.Animation;
+import graphic.Drawable;
+
+public class CobbleWall extends Drawable {
+	
+	private Animation anim;
+	
+	public CobbleWall(int x, int y, String wallType) {
+		super.setPosition(x, y);
+		anim = new Animation("map/Wall" + wallType);
+	}
+
+	@Override
+	public void onDraw(Graphics2D g) {
+		BufferedImage img = anim.getImage();
+		g.drawImage(img, 0, 0, null);
+	}
+
+}
---
+++ rpg/graphic/map/Grass.java
@@ -0,0 +1,24 @@
+package graphic.map;
+
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
+import feature.Animation;
+import graphic.Drawable;
+
+public class Grass extends Drawable {
+	
+	private Animation anim;
+	
+	public Grass(int x, int y) {
+		super.setPosition(x, y);
+		anim = new Animation("map/Grass");
+	}
+
+	@Override
+	public void onDraw(Graphics2D g) {
+		BufferedImage img = anim.getImage();
+		g.drawImage(img, 0, 0, null);
+	}
+
+}
