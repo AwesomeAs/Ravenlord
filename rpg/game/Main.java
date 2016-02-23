a9d6e0a56a5a7566974f983c5c2dec34174711ad
AwesomeAs <bloxxer9001@gmail.com>
2016-02-23 14:39:43 +0100

    Added credits page

--- rpg/game/Main.java
+++ rpg/game/Main.java
@@ -1,5 +1,6 @@
 package game;
 
+import graphic.Group;
 import graphic.Viewport;
 import graphic.map.*;
 import graphic.ui.*;
@@ -25,7 +26,47 @@ public class Main {
 			view.add(new CobbleWall(x, 512, (x > 64 ? (x < 800 - 128 ? "CBM" : "CBR") : "CBL")));
 		}
 		
-		view.add(new LButton("Test", view.getWidth() / 2 - 200, view.getHeight() / 2 - 30).setSize(400, 60));
+		Group main_g = new Group();
+		Group cred_g = new Group();
+		main_g.add(new LButton("Play game", view.getWidth() / 2 - 286, view.getHeight() / 2 - 82).setSize(572, 64).setCallback(new ButtonCallback() {
+
+			@Override
+			public void onClick() {
+				System.out.println("Play game");
+			}
+			
+		}));
+		main_g.add(new LButton("Settings", view.getWidth() / 2 - 286, view.getHeight() / 2 - 22).setSize(572, 64).setCallback(new ButtonCallback() {
+
+			@Override
+			public void onClick() {
+				System.out.println("Settings");
+			}
+			
+		}));
+		main_g.add(new LButton("Credits", view.getWidth() / 2 - 286, view.getHeight() / 2 + 42).setSize(572, 64).setCallback(new ButtonCallback() {
+
+			@Override
+			public void onClick() {
+				view.remove(main_g);
+				view.add(cred_g);
+			}
+			
+		}));
+		cred_g.add(new LButton("Main menu", view.getWidth() / 2 - 286, view.getHeight() / 2 + 100).setSize(572, 64).setCallback(new ButtonCallback() {
+
+			@Override
+			public void onClick() {
+				view.remove(cred_g);
+				view.add(main_g);
+			}
+			
+		}));
+		cred_g.add(new LText("This is a credits page\nHow's life? This seems fun, haha.\nI wonder if we can get text alignment.",
+				view.getWidth() / 2 - 286,
+				view.getHeight() / 2 - 160).setSize(572, 250));
+		cred_g.add(new LText("Credits", view.getWidth() / 2 - 286, view.getHeight() / 2 - 210, 48, 0f).setSize(572, 50));
+		view.add(main_g);
 	}
 
 }
