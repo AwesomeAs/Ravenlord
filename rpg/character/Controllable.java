8df4bf3cfaafd0b9c162dde10bb6af3558c894bd
Peter Bom Jakobsen <pejak1990@gmail.com>
2016-03-03 16:47:19 +0100

    Added more signatures to controllable.

--- rpg/character/Controllable.java
+++ rpg/character/Controllable.java
@@ -1,9 +1,18 @@
 package character;
 
-public interface Controllable {
+public interface Controllable { 
 	void jump();
 	void block();
 	void sprint();
-	void moveTo(double x, double y);
+	void moveTo(double x, double y, boolean relative);
 	void attack(Object... objects);
+	Direction getDirection();
+	void setDicrection(Direction direction);
+	boolean isWalking();
+	void setWalking(boolean walking);
+	float getWalkSpeed();
+	void setWalkSpeed(float speed);
+		
+	
+	enum Direction{LEFT, RIGHT, UP, DOWN};
 }
